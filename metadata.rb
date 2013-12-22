543b980ac72c060031c12bd73e4a0cf990beaff8
Thomas Orozco <thomas@orozco.fr>
2013-12-22 15:45:37 +0100

    Explain why we depend on iis.

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
 
 
-depends 'iis', '~> 1.5.0'
+depends 'iis', '~> 1.5.0'  # Required because 1.6.0 is broken, and php depends on it!
 depends 'php', '~> 1.3.0'
