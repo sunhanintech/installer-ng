071f2aadc29843825fd8ca6cc0118461287616f4
Thomas Orozco <thomas@orozco.fr>
2014-01-17 19:01:24 +0100

    Fix wrong mode for configuration file

--- recipes/configuration.rb
+++ recipes/configuration.rb
@@ -1,6 +1,6 @@
 template node[:scalr][:core][:configuration] do
   source "config.yml.erb"
-  mode 640
+  mode 0640
   owner node[:scalr][:core][:users][:service]
   group node[:scalr][:core][:group]
 end
