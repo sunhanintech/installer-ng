e1f82ab5b19a5e4bd6a8b40f99bccac829be5a89
Thomas Orozco <thomas@orozco.fr>
2014-01-17 17:43:43 +0100

    Reorder and add recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,26 @@
+# System
 include_recipe 'apt'
-include_recipe 'scalr-core::users'
-include_recipe 'scalr-core::package'
 include_recipe 'scalr-core::repos'
+
+# Users
+include_recipe 'scalr-core::users'
+
+# Scalr Code
+include_recipe 'scalr-core::package'
+
+# Scalr Configuration
+include_recipe 'scalr-core::configuration'
+
+# Runtime dependencies
 include_recipe 'scalr-core::php'
 include_recipe 'scalr-core::snmp'
+include_recipe 'scalr-core::scalrpy'
+include_recipe 'scalr-core::rrdcached'
 
+# Database Configuration
 include_recipe 'scalr-core::database'
+
+# Service Launch
+include_recipe 'scalr-core::web'
+include_recipe 'scalr-core::services'
+include_recipe 'scalr-core::cron'
