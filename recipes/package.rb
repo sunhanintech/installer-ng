8e4a8cfd85ac20a8844e590ea300c6727ff64436
Thomas Orozco <thomas@orozco.fr>
2014-01-22 21:17:13 +0100

    Fix user for Scalr package

--- recipes/package.rb
+++ recipes/package.rb
@@ -5,8 +5,8 @@ artifact_deploy node[:scalr][:core][:package][:name] do
   artifact_checksum node[:scalr][:core][:package][:checksum]
   version node[:scalr][:core][:package][:version]
   deploy_to node[:scalr][:core][:package][:deploy_to]
-  owner 'root'
-  group 'root'
+  owner node[:scalr][:core][:users][:service]
+  group node[:scalr][:core][:group]
 end
 
 
