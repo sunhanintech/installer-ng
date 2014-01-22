136bd7a915ff696e3774312fc11876fbf3645d0d
Thomas Orozco <thomas@orozco.fr>
2014-01-22 21:17:56 +0100

    Issue with http PECL only happens on Debian

--- recipes/php_pecl_http.rb
+++ recipes/php_pecl_http.rb
@@ -16,11 +16,14 @@ php_pear 'pecl_http' do  # Can't I just override the provider here?
 end
 
 # Php Pear does not recognize this as a PECL.
-cookbook_file "#{node['php']['ext_conf_dir']}/http.ini" do
-  owner 'root'
-  group 'root'
-  mode 0644
-  source "http.ini"
+# Only on ... debian.
+if node[:platform_family] == 'debian'
+  cookbook_file "#{node['php']['ext_conf_dir']}/http.ini" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    source "http.ini"
+  end
 end
 
 scalr_core_phpmod 'http'
