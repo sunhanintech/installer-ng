7cf036ca9372f8a10d33f6e52f3bfe5dc30c9dc5
Thomas Orozco <thomas@orozco.fr>
2014-01-29 23:33:45 +0100

    Fix libyaml installation on RHEL with cloud-init

--- recipes/php_pecl_yaml.rb
+++ recipes/php_pecl_yaml.rb
@@ -1,6 +1,21 @@
 case node[:platform_family]
 
 when 'rhel'
+  # When cloud-init is installed, RHEL has libyaml installed,
+  # but it's tagged release: 1.1.el6. This prevents us from installing
+  # libyaml-devel from epel, which is tagged release: 1.el6 and depends
+  # on libyaml itself.
+  #
+  # Unfortunately, Red Hat does not ship libyaml-devel, so we have to
+  # uninstall libyaml to then be able to install libyaml-devel.
+  #
+  # libyaml isn't installed through a repo, so we just need to check
+  # whether there's an override, and downgrade if there is.
+
+  execute 'yum --assumeyes downgrade libyaml' do
+    only_if 'yum info libyaml | grep koji-override-0'
+  end
+
   package 'libyaml-devel'
 when 'debian'
   package 'libyaml-dev'
