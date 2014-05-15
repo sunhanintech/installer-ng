481de5fb18f2c91b3cdd68bb44219f019573dce9
Thomas Orozco <thomas@orozco.fr>
2014-05-15 13:49:53 -0400

    Use new upload location

--- release.sh
+++ release.sh
@@ -19,42 +19,36 @@ exit_dirty_files () {
 git diff-files --quiet || exit_dirty_files
 
 
-TOKEN_FILE="$HOME/.installer-ng-deploy-token"
-touch $TOKEN_FILE
-token=`cat $TOKEN_FILE`
-if [ -z $token ]; then
-  echo "No Github token found - add it to $TOKEN_FILE"
-  exit 1
-fi
-
 # Valid release - start.
 echo "Creating release $release"
 
-
-METADATA_FILE="metadata.rb"
-INSTALL_FILE="scripts/install.py"
-
-sed -E -i '' "s/(version[ ]+)'[0-9.]*'/\1'$release'/g" $METADATA_FILE
-sed -E -i '' "s/(COOKBOOK_VERSION[ ]+=[ ]+)\"[0-9.]*\"/\1\"$release\"/g" $INSTALL_FILE
-
-
 echo "Creating release branch"
-release_branch="release-$release"
-git checkout -b $release_branch
-git add $METADATA_FILE $INSTALL_FILE
-git commit -m "Release: $release"
-git tag "v$release" HEAD
+RELEASE_NAME="v$release"
+RELEASE_BRANCH="release-$release"
 
+make_local_release () {
+  metadata_file="metadata.rb"
+  install_file="scripts/install.py"
+
+  sed -E -i '' "s/(version[ ]+)'[0-9.]*'/\1'$release'/g" $metadata_file
+  sed -E -i '' "s/(COOKBOOK_VERSION[ ]+=[ ]+)\"[0-9.]*\"/\1\"$release\"/g" $install_file
+
+  git checkout -b $RELEASE_BRANCH
+  git add $metadata_file $install_file
+  git commit -m "Release: $release"
+  git tag $RELEASE_NAME HEAD
+}
+
+git tag | grep $RELEASE_NAME || make_local_release
 
 echo "Pushing release branch"
-git push origin $release_branch:$release_branch
+git push origin $RELEASE_BRANCH:$RELEASE_BRANCH
 git push --tags
-
 
 RELEASE_DIR="$TMPDIR/installer-ng-release-$release-$$"
 PACKAGE_NAME="package.tar.gz"
 PACKAGE_FILE="$RELEASE_DIR/$PACKAGE_NAME"
-
+RELEASE_PACKAGE_FILE=$RELEASE_DIR/installer-ng-$RELEASE_NAME.tar.gz
 
 echo "Releasing in $RELEASE_DIR"
 if [ -z $RELEASE_DIR ]; then
@@ -72,18 +66,9 @@ echo "Creating release package"
 cd $RELEASE_DIR
 berks pack
 
-
-echo "Creating Github release"
-
-INSTALLER_REPO="scalr/installer-ng"
-create_release_json=$(printf '{"tag_name": "v%s", "name": "v%s", "body": "Pre-release: %s", "draft": false, "prerelease": true}' $release $release $release)
-response=$(curl --fail --data "$create_release_json" "https://api.github.com/repos/$INSTALLER_REPO/releases?access_token=$token")
-release_id=$(echo $response | jq ".id")
-echo "Created release v$release: $release_id"
-
-echo "Uploading $PACKAGE_FILE"
-response=$(curl --fail -X POST -H "Content-Type:application/gzip" --upload-file $PACKAGE_FILE "https://uploads.github.com/repos/$INSTALLER_REPO/releases/$release_id/assets?name=$PACKAGE_NAME&access_token=$token")
-
-echo "Received response $response"
+# Upload the release in S3
+echo "Uploading to S3"
+mv $PACKAGE_FILE $RELEASE_PACKAGE_FILE
+s3put --bucket=installer.scalr.com --prefix=$(dirname $RELEASE_PACKAGE_FILE) --key_prefix="releases" --grant=public-read --callback=10 $RELEASE_PACKAGE_FILE
 
 echo "Done"
