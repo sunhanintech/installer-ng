2f82642473d87d425bcc8ce848a2408a3db3182b
Thomas Orozco <thomas@orozco.fr>
2014-05-19 10:23:49 +0200

    Move cron definition to attributes file

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -1,100 +1,14 @@
 #TODO: Reduce duplication user
 php = '/usr/bin/php -q'
-cmd = "#{php} #{node[:scalr][:core][:location]}/app/cron/cron.php"
+og_cmd = "#{php} #{node[:scalr][:core][:location]}/app/cron/cron.php"
 ng_cmd = "#{php} #{node[:scalr][:core][:location]}/app/cron-ng/cron.php"
 
-cron "Scheduler" do
-  user node[:scalr][:core][:users][:service]
-  command "#{cmd} --Scheduler"
-end
-
-cron "UsageStatsPoller" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/5"
-  command "#{cmd} --UsageStatsPoller"
-end
-
-cron "Scaling" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/2"
-  command "#{ng_cmd} --Scaling"
-end
-
-if node[:scalr][:is_enterprise]
-  messaging_crons = %w{
-    SzrMessagingAll SzrMessagingAll2
-    SzrMessagingBeforeHostUp SzrMessagingBeforeHostUp2
-    SzrMessagingHostInit SzrMessagingHostInit2
-    SzrMessagingHostUp SzrMessagingHostUp2
-  }
-else
-  messaging_crons = %w{SzrMessaging}
-end
-
-messaging_crons.each do |messaging_cron|
-  cron messaging_cron do
+node[:scalr][:crons].each do |cron|
+  cmd = cron[:ng] ? ng_cmd : og_cmd
+  cron cron[:name] do
     user node[:scalr][:core][:users][:service]
-    minute "*/2"
-    command "#{cmd} --#{messaging_cron}"
+    hour    cron[:hour]
+    minute  cron[:minute]
+    command "#{cmd} --#{cron[:name]}"
   end
-end
-
-cron "BundleTasksManager" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/2"
-  command "#{cmd} --BundleTasksManager"
-end
-
-cron "MetricCheck" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/15"
-  command "#{ng_cmd} --MetricCheck"
-end
-
-cron "Poller" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/2"
-  command "#{ng_cmd} --Poller"
-end
-
-cron "DNSManagerPoll" do
-  user node[:scalr][:core][:users][:service]
-  command "#{cmd} --DNSManagerPoll"
-end
-
-cron "RotateLogs" do
-  user node[:scalr][:core][:users][:service]
-  minute "17"
-  hour "5"
-  command "#{cmd} --RotateLogs"
-end
-
-cron "EBSManager" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/2"
-  command "#{cmd} --EBSManager"
-end
-
-cron "RolesQueue" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/20"
-  command "#{cmd} --RolesQueue"
-end
-
-cron "DbMsrMaintenance" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/5"
-  command "#{ng_cmd} --DbMsrMaintenance"
-end
-
-cron "LeaseManager" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/20"
-  command "#{ng_cmd} --LeaseManager"
-end
-
-cron "ServerTerminate" do
-  user node[:scalr][:core][:users][:service]
-  minute "*/1"
-  command "#{ng_cmd} --ServerTerminate"
 end
