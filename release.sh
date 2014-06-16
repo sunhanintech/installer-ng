4de576b946d755a017c36d8b0cd867e0769be47d
Thomas Orozco <thomas@orozco.fr>
2014-06-16 16:14:33 +0200

    Optionally do not publish releases

--- release.sh
+++ release.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-release=$1
-
 ORIGINAL_DIR=$(pwd)
 ORIGINAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 set -o errexit
 set -o nounset
+
+OPTIND=1
+
+no_push=
+
+while getopts "x" opt; do
+  case "$opt" in
+    x)
+      no_push=1
+      ;;
+  esac
+done
+
+shift "$((OPTIND-1))"
+release=$1
+
 
 cd $( dirname "${BASH_SOURCE[0]}" )
 
@@ -47,10 +61,6 @@ make_local_release () {
 
 git tag | grep --extended-regexp "^$RELEASE_NAME$" || make_local_release
 
-echo "Pushing release branch"
-git push origin $RELEASE_BRANCH:$RELEASE_BRANCH
-git push --tags
-
 RELEASE_DIR="$TMPDIR/installer-ng-release-$release-$$"
 PACKAGE_NAME="package.tar.gz"
 PACKAGE_FILE="$RELEASE_DIR/$PACKAGE_NAME"
@@ -78,6 +88,16 @@ mv $PACKAGE_FILE $RELEASE_PACKAGE_FILE
 s3put --bucket=installer.scalr.com --prefix=$(dirname $RELEASE_PACKAGE_FILE) --key_prefix="releases" --grant=public-read --callback=10 $RELEASE_PACKAGE_FILE
 
 cd $ORIGINAL_DIR
+
+if [ -z "$no_push" ]; then
+  echo "Pushing release branch"
+  git push origin $RELEASE_BRANCH:$RELEASE_BRANCH
+  git push --tags
+else
+  echo "Not pushing release branch: -x is set"
+fi
+
 git checkout $ORIGINAL_BRANCH
 
-echo "Done. Released: $release"
+echo "Done. Published: $release"
+
