66dea218ddbc7345023431c48de9b1d34c3c67ef
Thomas Orozco <thomas@orozco.fr>
2014-08-25 15:47:59 +0200

    Add fix for database initial data loading in OSS
    The upgrades table is empty in OSS Scalr, so looking at it to tell
    whether we should be loading data isn't a good solution!

--- recipes/database.rb
+++ recipes/database.rb
@@ -41,15 +41,25 @@ base_conn_params = "-h'#{node[:scalr][:database][:host]}' -u'#{node[:scalr][:dat
 scalr_conn_params = "#{base_conn_params} -D'#{node[:scalr][:database][:scalr_dbname]}'"
 analytics_conn_params = "#{base_conn_params} -D'#{node[:scalr][:database][:analytics_dbname]}'"
 
+# Load the DB structure only if the upgrades table cannot be found (it's in the structure)
 execute "Load Scalr Database Structure" do
   command "mysql #{scalr_conn_params} < #{node[:scalr][:core][:location]}/sql/structure.sql"
   not_if "[ $(mysql #{base_conn_params} -Ns -e \"SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='#{node[:scalr][:database][:scalr_dbname]}' AND table_name='upgrades';\") -gt 0 ]"
   # Only import structure if the upgrades table is not there yet (because it's always in the structure file)
 end
 
+
+# Load data only if no upgrade data is there (>= 5.0), or rely on some other indicator if upgrade data is unavailable (< 5.0)
+
+if Gem::Dependency.new(nil, '~> 5.0').match?(nil, node.scalr.package.version)
+  data_test_query = "SELECT COUNT(*) FROM upgrades;"
+else
+  data_test_query = "SELECT COUNT(*) FROM ipaccess;"
+end
+
 execute "Load Scalr Database Data" do
   command "mysql #{scalr_conn_params} < #{node[:scalr][:core][:location]}/sql/data.sql"
-  not_if "[ $(mysql #{scalr_conn_params} -Ns -e \"SELECT COUNT(*) FROM upgrades;\") -gt 0 ]"
+  not_if "[ $(mysql #{scalr_conn_params} -Ns -e \"#{data_test_query}\") -gt 0 ]"
   # Only import data if it's none at this point.
 end
 
