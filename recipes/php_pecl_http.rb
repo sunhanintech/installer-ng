efc930cf178af3d2e5c22370908552fc725f6b87
Thomas Orozco <thomas@orozco.fr>
2014-09-01 20:17:08 +0200

    RHEL7: Remove file/libmagic headers installation
    We're not using content-type guessing here anyway.

--- recipes/php_pecl_http.rb
+++ recipes/php_pecl_http.rb
@@ -1,12 +1,10 @@
 case node[:platform_family]
 
 when 'rhel'
-  package 'file-devel'
   package 'zlib-devel'
   package 'curl-devel'
 when 'debian'
   package 'zlib1g-dev'
-  package 'libmagic-dev'
   package 'libcurl4-openssl-dev'
 end
 
