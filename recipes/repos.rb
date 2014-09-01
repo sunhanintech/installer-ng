f7619e045d42a526f56d991a530923ca4f019703
Thomas Orozco <thomas@orozco.fr>
2014-09-01 19:58:29 +0200

    RHEL7: Webtatic has new dependencies on EPEL

--- recipes/repos.rb
+++ recipes/repos.rb
@@ -7,11 +7,21 @@ when 'rhel', 'fedora'
   # Since we are using platform_version, might as well use it everywhere.
   rhel_version = node[:platform_version].to_i
 
+  if rhel_version == 7
+    webtatic_artifact = '-testing'  # TODO - Use regular repository once webtatic moves PHP out of testing.
+    webtatic_gpg_key = 'http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-el7'
+    webtatic_epel_deps = 'libmcrypt*'  # in RHEL 7 - Webtatic starts depending on EPEL for packages
+  else
+    webtatic_artifact = ''
+    webtatic_gpg_key = 'http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-andy'
+    webtatic_epel_deps = ''
+  end
+
   yum_repository 'epel' do
     description 'Extra Packages for Enterprise Linux'
     mirrorlist "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-#{rhel_version}&arch=$basearch"
     gpgkey "https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-#{rhel_version}"
-    includepkgs 'libyaml* monit*'
+    includepkgs "libyaml* monit* #{webtatic_epel_deps}"
     action :create
   end
 
@@ -31,15 +41,12 @@ when 'rhel', 'fedora'
     action :create
   end
 
-  # TODO - Use regular repository once webtatic moves PHP out of testing.
-  webtatic_artifact = rhel_version == 7 ? "-testing" : ""
-
   yum_repository 'webtatic' do
     description "Webtatic Repository #{rhel_version} - $basearch"
-    mirrorlist "http://mirror.webtatic.com/yum/el#{rhel_version}#{webtatic_artifact}/$basearch/mirrorlist"
-    gpgkey rhel_version == 7 ? 'http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-el7' : 'http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-andy'
+    mirrorlist  "http://mirror.webtatic.com/yum/el#{rhel_version}#{webtatic_artifact}/$basearch/mirrorlist"
+    gpgkey      webtatic_gpg_key
     includepkgs 'libmysqlclient* libmcrypt* php*'
-    action :create
+    action      :create
   end
 
 when 'debian'  #TODO: Find out how we support Debian here?
