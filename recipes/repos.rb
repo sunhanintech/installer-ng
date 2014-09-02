c966214b863fc060cf83568887b73b85d966578b
Thomas Orozco <thomas@orozco.fr>
2014-09-02 13:41:09 +0200

    Rearchitecture RHEL / CentOS repository handling
    We have a very different repository configuration for RHEL 7 from what
    we have on RHEL 6. Rearchitect the code to reflect this difference.

--- recipes/repos.rb
+++ recipes/repos.rb
@@ -11,14 +11,27 @@ when 'rhel', 'fedora'
   # Since we are using platform_version, might as well use it everywhere.
   rhel_version = node[:platform_version].to_i
 
+  webtatic_pkgs         = ['libmysqlclient*', 'php*']
+  epel_pkgs             = ['monit*']
+  rpmforge_pkgs         = ['putty*']
+  rpmforge_extras_pkgs  = []
+  centos_base_pkgs      = []
+
   if rhel_version == 7
+    epel_pkgs.concat        ['libmcrypt*']
+    centos_base_pkgs.concat ['rrdtool*', 'file*', 'libssh2*', 'libyaml*']
+
+    # Webtatic artifacts..
     webtatic_artifact = '-testing'  # TODO - Use regular repository once webtatic moves PHP out of testing.
     webtatic_gpg_key = 'http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-el7'
-    webtatic_epel_deps = 'libmcrypt*'  # in RHEL 7 - Webtatic starts depending on EPEL for packages
   else
+    webtatic_pkgs.concat        ['libmcrypt*']
+    epel_pkgs.concat            ['libyaml*']
+    rpmforge_extras_pkgs.concat ['rrdtool*']
+
+    # Webtatic artifacts..
     webtatic_artifact = ''
     webtatic_gpg_key = 'http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-andy'
-    webtatic_epel_deps = ''
   end
 
   # Webtatic is where we can find up-to-date PHP packages.
@@ -26,7 +39,7 @@ when 'rhel', 'fedora'
     description "Webtatic Repository #{rhel_version} - $basearch"
     mirrorlist  "http://mirror.webtatic.com/yum/el#{rhel_version}#{webtatic_artifact}/$basearch/mirrorlist"
     gpgkey      webtatic_gpg_key
-    includepkgs 'libmysqlclient* libmcrypt* php*'
+    includepkgs webtatic_pkgs.join(' ')
     action      :create
   end
 
@@ -35,29 +48,29 @@ when 'rhel', 'fedora'
   # Note that libyaml is in base on CentOS 7, and no longer in EPEL!
   yum_repository 'epel' do
     description 'Extra Packages for Enterprise Linux'
-    mirrorlist "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-#{rhel_version}&arch=$basearch"
-    gpgkey "https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-#{rhel_version}"
-    includepkgs "libyaml* monit* #{webtatic_epel_deps}"
-    action :create
+    mirrorlist  "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-#{rhel_version}&arch=$basearch"
+    gpgkey      "https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-#{rhel_version}"
+    includepkgs epel_pkgs.join(' ')
+    action      :create
   end
 
   # We need putty, which we can find in repoforge.
   yum_repository 'rpmforge' do
     description "RHEL #{rhel_version} - RPMforge.net - base"
-    mirrorlist "http://apt.sw.be/redhat/el#{rhel_version}/en/mirrors-rpmforge"
-    gpgkey 'http://apt.sw.be/RPM-GPG-KEY.dag.txt'
-    includepkgs 'putty'
-    action :create
+    mirrorlist  "http://apt.sw.be/redhat/el#{rhel_version}/en/mirrors-rpmforge"
+    gpgkey      'http://apt.sw.be/RPM-GPG-KEY.dag.txt'
+    includepkgs rpmforge_pkgs.join(' ')
+    action      :create
   end
 
   # We need an up-to-date rrdtool version that includes rrcached. We can find this in
   # repoforge-extras.
   yum_repository 'rpmforge-extras' do
     description "RHEL #{rhel_version} - RPMforge.net - extras"
-    mirrorlist "http://apt.sw.be/redhat/el#{rhel_version}/en/mirrors-rpmforge-extras"
-    gpgkey 'http://apt.sw.be/RPM-GPG-KEY.dag.txt'
-    includepkgs '*rrdtool*'
-    action :create
+    mirrorlist  "http://apt.sw.be/redhat/el#{rhel_version}/en/mirrors-rpmforge-extras"
+    gpgkey      'http://apt.sw.be/RPM-GPG-KEY.dag.txt'
+    includepkgs rpmforge_extras_pkgs.join(' ')
+    action      :create
   end
 
   # The Red Hat repositories are missing -devel packages that we need
@@ -67,8 +80,8 @@ when 'rhel', 'fedora'
       description 'CentOS-$releasever - Base'
       mirrorlist  "http://mirrorlist.centos.org/?release=#{rhel_version}&arch=$basearch&repo=os"
       gpgkey      "http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-#{rhel_version}"
-      includepkgs 'file* libssh2* libyaml*'
-      action :create
+      includepkgs centos_base_pkgs.join(' ')
+      action      :create
     end
   end
 
