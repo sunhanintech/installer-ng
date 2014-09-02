d32e86fe9644a0dc7e5744a9b6cf5d38cfd88eb9
Thomas Orozco <thomas@orozco.fr>
2014-09-02 14:45:57 +0200

    Upgrade Apache Cookbook to 2.0.0

--- recipes/web.rb
+++ recipes/web.rb
@@ -1,23 +1,4 @@
-if node[:platform_family] == 'debian'
-  # We need to update the pid dir here, or we'll be unable to stop Apache.
-  # We can't instruct the cookbook to ignore that failure and killall instead,
-  # so here goes.
-  template "#{node['apache']['dir']}/envvars" do
-    source "apache2-envvars.erb"
-  end
-
-end
-
 include_recipe 'apache2'
-
-t = resources("template[apache2.conf]")
-t.source "#{node[:platform_family]}-apache2.conf.erb"
-t.cookbook "scalr-core"
-
-node['apache']['extra_modules'].each do |mod|
-  # Those don't have recipes in the apache2 cookbook
-  apache_module mod
-end
 
 web_app 'scalr' do
   template 'scalr-vhost.conf.erb'
