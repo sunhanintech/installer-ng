ebca57a05277a598bc22f336bbdf379d5d0c1f66
Thomas Orozco <thomas@orozco.fr>
2014-09-02 11:43:23 +0200

    Include the CentOS repositories on RHEL 7
    On RHEL 7, it seems that some *-devel packages are no longer available
    in the base RHEL repositories.
    
    However, those packages are available in CentOS 7's repositories, so we
    use those instead.

--- recipes/repos.rb
+++ recipes/repos.rb
@@ -1,6 +1,10 @@
 case node[:platform_family]
 
 when 'rhel', 'fedora'
+  # We need to use some extra repositories to pull specific packages
+  # In order to not mess up the entire system, we use includepks to
+  # ensure that only the packages we want are installed.
+
   # TODO - Use HTTPS or a file for gpgkey's!
 
   # EPEL uses a different key on 6 and 7.
@@ -17,6 +21,18 @@ when 'rhel', 'fedora'
     webtatic_epel_deps = ''
   end
 
+  # Webtatic is where we can find up-to-date PHP packages.
+  yum_repository 'webtatic' do
+    description "Webtatic Repository #{rhel_version} - $basearch"
+    mirrorlist  "http://mirror.webtatic.com/yum/el#{rhel_version}#{webtatic_artifact}/$basearch/mirrorlist"
+    gpgkey      webtatic_gpg_key
+    includepkgs 'libmysqlclient* libmcrypt* php*'
+    action      :create
+  end
+
+  # We'll need EPEL for libyaml and monit. Webtatic has started depending on EPEL in RHEL 7, so
+  # we also need to include those dependencies here.
+  # Note that libyaml is in base on CentOS 7, and no longer in EPEL!
   yum_repository 'epel' do
     description 'Extra Packages for Enterprise Linux'
     mirrorlist "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-#{rhel_version}&arch=$basearch"
@@ -25,6 +41,7 @@ when 'rhel', 'fedora'
     action :create
   end
 
+  # We need putty, which we can find in repoforge.
   yum_repository 'rpmforge' do
     description "RHEL #{rhel_version} - RPMforge.net - base"
     mirrorlist "http://apt.sw.be/redhat/el#{rhel_version}/en/mirrors-rpmforge"
@@ -33,6 +50,8 @@ when 'rhel', 'fedora'
     action :create
   end
 
+  # We need an up-to-date rrdtool version that includes rrcached. We can find this in
+  # repoforge-extras.
   yum_repository 'rpmforge-extras' do
     description "RHEL #{rhel_version} - RPMforge.net - extras"
     mirrorlist "http://apt.sw.be/redhat/el#{rhel_version}/en/mirrors-rpmforge-extras"
@@ -41,12 +60,16 @@ when 'rhel', 'fedora'
     action :create
   end
 
-  yum_repository 'webtatic' do
-    description "Webtatic Repository #{rhel_version} - $basearch"
-    mirrorlist  "http://mirror.webtatic.com/yum/el#{rhel_version}#{webtatic_artifact}/$basearch/mirrorlist"
-    gpgkey      webtatic_gpg_key
-    includepkgs 'libmysqlclient* libmcrypt* php*'
-    action      :create
+  # The Red Hat repositories are missing -devel packages that we need
+  # $releasever?
+  if node[:platform] == 'redhat'
+    yum_repository 'centos-base' do
+      description 'CentOS-$releasever - Base'
+      mirrorlist  "http://mirrorlist.centos.org/?release=#{rhel_version}&arch=$basearch&repo=os"
+      gpgkey      "http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-#{rhel_version}"
+      includepkgs 'file* libssh2* libyaml*'
+      action :create
+    end
   end
 
 when 'debian'  #TODO: Find out how we support Debian here?
