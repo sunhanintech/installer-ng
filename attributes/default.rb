3a46202f542ac1b931b513b7e2268ab255f010e6
Thomas Orozco <thomas@orozco.fr>
2014-09-02 17:10:46 +0200

    Use a custom directory for session data
    This gives us more flexibility w.r.t. permissions (we can make sure
    it's owned by the same user / group as Apache).
    
    This change is needed become as of the upgrade of the ugprade to the
    Apache Cookbook 2.0.0, we changed the group (it used to be www-data /
    apache, now it's scalr) that the Apache process runs as.

--- attributes/default.rb
+++ attributes/default.rb
@@ -154,14 +154,14 @@ when 'rhel', 'fedora'
   # View here for package contents (extensions): https://webtatic.com/packages/php55/
   default['php']['packages'] = %w{php55w php55w-devel php55w-cli php55w-mysql php55w-mcrypt php55w-snmp php55w-process php55w-xml php55w-soap php55w-pear}
   default['php']['cnf_dirs'] = %w{/etc/php.d}
-  default['php']['session_save_path'] = '/var/lib/php/session'
 when 'debian'
   # View here for package contents (extensions): http://ppa.launchpad.net/ondrej/php5/ubuntu/dists/precise/main/binary-amd64/Packages
   default['php']['packages'] = %w{php5 php5-dev php5-mysql php5-mcrypt php5-curl php5-snmp php-pear}
   default['php']['cnf_dirs'] = %w{/etc/php5/apache2/conf.d /etc/php5/cli/conf.d}
   default['php']['ext_conf_dir'] = '/etc/php5/mods-available'
-  default['php']['session_save_path'] = '/var/lib/php5/sessions'
 end
+
+default['php']['session_save_path'] = '/var/lib/scalr/sessions'
 
 
 # Apache attributes
