9ff7f0580001ed41f7ec0bfc6a14b26d061f5406
Thomas Orozco <thomas@orozco.fr>
2014-10-17 12:58:32 +0200

    Release: 6.3.1

--- scripts/install.py
+++ scripts/install.py
@@ -31,7 +31,7 @@ CHEF_RUBY_BIN = "/opt/chef/embedded/bin/ruby"
 MINIMUM_CHEF_VERSION = "11.0.0"
 MINIMUM_RUBY_VERSION = "1.9.0"
 
-DEFAULT_COOKBOOK_RELEASE = "6.3.0"
+DEFAULT_COOKBOOK_RELEASE = "6.3.1"
 COOKBOOK_PKG_URL_FORMAT = "https://s3.amazonaws.com/installer.scalr.com/releases/installer-ng-v{0}.tar.gz"
 
 INSTALLER_UMASK = 0o22
