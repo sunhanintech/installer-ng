9e2944e9149eaf907e56156ace074c322aa2755b
Thomas Orozco <thomas@orozco.fr>
2014-11-03 17:48:29 +0100

    Don't create users / DBs on non MySQL host

--- recipes/group_mysql.rb
+++ recipes/group_mysql.rb
@@ -19,3 +19,31 @@ template "/etc/mysql/conf.d/tz.cnf" do
   group "root"
   notifies :restart, "mysql_service[#{node['mysql']['service_name']}]", :delayed
 end
+
+mysql_database_user node[:scalr][:database][:username] do
+  connection root_conn_info
+
+  password node[:scalr][:database][:password]
+  host node[:scalr][:database][:client_host]
+
+  action [:create]
+end
+
+scalr_databases = [node[:scalr][:database][:scalr_dbname]]
+if Gem::Dependency.new(nil, '~> 5.0').match?(nil, node.scalr.package.version)
+  scalr_databases.push(node[:scalr][:database][:analytics_dbname])
+end
+
+scalr_databases.each do |scalr_database|
+  mysql_database scalr_database do
+    connection root_conn_info
+    action :create
+  end
+
+  mysql_database_user node[:scalr][:database][:username] do
+    connection root_conn_info
+    database_name scalr_database
+    action [:grant]
+  end
+end
+
