325abeff2677663e286ddcb8a027bcbf2603cd82
Thomas Orozco <thomas@orozco.fr>
2014-11-12 00:40:18 +0100

    Move stuff around — all deb build packages

---
+++ wrapper/build/build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+# Docker doesn't make our lives easy with SSH, because UIDs don't match
+# To work around this, we only publish the key to the container, no SSH config file,
+# or anything else.
+: ${SSH_KEY:="id_rsa"}
+
+
+REL_HERE=$(dirname "${BASH_SOURCE}")
+HERE=$(cd "${REL_HERE}"; pwd)  # Get an absolute path
+PKG_DIR="$(dirname $HERE)/scalr-installer"
+
+# First, build the package. This is somewhat hackish, but it's so we can give it to
+# Docker easily. We have to do this because directly sharing the package volume is
+# a performance disaster when using e.g. boot2docker, which is exactly why we have
+# this script. It's slow because we deal with plenty of small files.
+
+python -c "import setuptools_git" || {
+  echo "ERROR: setuptools_git is required for this build to work! Install it."
+  exit 1
+}
+cd $PKG_DIR
+PKG_VERSION=$(python -c "exec(compile(open('scalr_installer/version.py').read(), 'version.py', 'exec')); print __version__")
+python setup.py sdist
+
+# Now, let's inject the archive into all our build contexts!
+PKG_ARCHIVE="$PKG_DIR/dist/scalr-installer-${PKG_VERSION}.tar.gz"
+
+# Now, build the "binary" packages, in each builder we have
+FACTORY_BASE_NAME=installer/factory
+
+delete_files=""
+cleanup_on_exit () {
+  echo "Removing: $delete_files"
+  if [[ -n "$delete_files" ]]; then
+   rm -- $delete_files
+  fi
+}
+trap cleanup_on_exit INT TERM EXIT
+
+cd $HERE
+
+for builderDir in *; do
+  if [[ -d $builderDir ]]; then
+    # First, check this is in fact a builder directory, not just a random script!
+    img="${FACTORY_BASE_NAME}-$builderDir"
+    build_pkg="$builderDir/pkg.tar.gz"
+
+    cp "$PKG_ARCHIVE" "$build_pkg"
+    delete_files="$delete_files $build_pkg"
+
+    echo "Building $img"
+    docker build -t $img "$builderDir"
+    docker run -it -v ~/.gnupg:/root/.gnupg -v ~/.ssh/$SSH_KEY:/root/.ssh/$SSH_KEY -e PKG_DIR=/build/scalr-installer-$PKG_VERSION "$img"
+  fi
+done
+
---
+++ wrapper/build/deb/tools/deb_build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+: ${DEB_VERSION:="1"}
+: ${DIST_DIR:="/dist"}
+
+# Load agents
+eval "$(ssh-agent -s)"
+eval "$(gpg-agent --daemon --sh)"
+
+# Load all SSH Keys
+ssh-add ~/.ssh/*
+
+# Start the build
+cd $PKG_DIR
+
+PKG_VERSION=$(python -c "exec(compile(open('scalr_installer/version.py').read(), 'version.py', 'exec')); print __version__")
+
+# Fist, actually building the orig.tar.gz — we need to build it once, otherwise future builds will fail
+python setup.py sdist --dist-dir=$DIST_DIR
+orig=${DIST_DIR}/scalr-installer-${PKG_VERSION}.tar.gz
+
+# Extract the version
+
+for release in precise raring trusty; do
+  full_version="$DEB_VERSION~$release"
+  python setup.py sdist_dsc --use-premade-distfile=$orig --debian-version=$full_version --suite=$release --dist-dir=$DIST_DIR
+
+  changes=${DIST_DIR}/scalr-installer_${PKG_VERSION}-${full_version}_source.changes
+  debsign $changes
+  dput -c $TOOLS_DIR/dput.cf scalr-installer $changes
+done
+
