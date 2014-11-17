f2f3c660e2aa7b88d63d4f85897d979669b816a8
Thomas Orozco <thomas@orozco.fr>
2014-11-17 14:15:10 +0100

    Print document to stdout

--- wrapper/scalr-manage/scalr_manage/library/document/test/test_document.py
+++ wrapper/scalr-manage/scalr_manage/library/document/test/test_document.py
@@ -3,15 +3,23 @@ from __future__ import unicode_literals
 
 import os
 import unittest
-import testfixtures
 
 from scalr_manage.library.document.target import DocumentTarget
+from scalr_manage.rnd import RandomTokenGenerator
+from scalr_manage.ui.engine import UserInput
 
 from scalr_manage.test.util import TestParser
+from scalr_manage.ui.test.util import MockOutput, MockInput
 
 
 class DocumentTestCase(unittest.TestCase):
     def setUp(self):
+        self.input = MockInput()
+        self.output = MockOutput()
+        self.ui = UserInput(self.input, self.output)
+
+        self.tokgen = RandomTokenGenerator(os.urandom)
+
         self.parser = TestParser()
 
         self.test_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data")
@@ -26,18 +34,16 @@ class DocumentTestCase(unittest.TestCase):
 
 
         target = DocumentTarget()
-        with testfixtures.LogCapture() as l:
-            target(args, None, None )
+        target(args, self.ui, self.tokgen)
 
-            actual = [r.getMessage() for r in l.records]
-            for expected in [
-                "Scalr is installed at: `/opt/scalr`",
-                "Launch Scalr by browsing to `http://app.scalr.test`",
-                "Username: `admin`",
-                "Password: `scalrpass`",
-                "Your scalr installation ID is: `i0xaaaaaaaa`",
-                "`scalr`: `dbpass`",
+        for expected in [
+            "Scalr is installed at: `/opt/scalr`",
+            "Launch Scalr by browsing to `http://app.scalr.test`",
+            "Username: `admin`",
+            "Password: `scalrpass`",
+            "Your scalr installation ID is: `i0xaaaaaaaa`",
+            "`scalr`: `dbpass`",
             ]:
-                self.assertTrue(expected in actual)
+            self.assertTrue(expected in self.output.outputs)
 
 
