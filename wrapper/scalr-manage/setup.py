743ef85f2e70667d197ff5b3e5fbcd2f3e27086a
Thomas Orozco <thomas@orozco.fr>
2014-11-17 12:48:59 +0100

    Stop using setuptools_git

--- wrapper/scalr-manage/setup.py
+++ wrapper/scalr-manage/setup.py
@@ -13,6 +13,54 @@ with open(os.path.join(here, PROJECT_DIR, "version.py")) as f:
     code = compile(f.read(), "version.py", "exec")
     exec(code)
 
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join)
+    in a platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+
+def is_package(package_name):
+    for pkg in EXCLUDE_FROM_PACKAGES:
+        if package_name.startswith(pkg):
+            return False
+    return True
+
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, package_data = [], {}
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+django_dir = 'scalr_manage'
+
+for dirpath, dirnames, filenames in os.walk(django_dir):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames and is_package(package_name):
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
+
 dependencies = ["requests", "setuptools", "jinja2"]
 if sys.version_info < (2, 7, 0):
     dependencies.append("argparse")
@@ -20,8 +68,8 @@ if sys.version_info < (2, 7, 0):
 setuptools.setup(
     name="scalr-manage",
     version=__version__,
-    packages=setuptools.find_packages(),
-    include_package_data=True,
+    packages=packages,
+    package_data=package_data,
     zip_safe=False,
     license="Apache Software License 2.0",
     author="Thomas Orozco",
@@ -33,7 +81,6 @@ setuptools.setup(
         ]
     },
     install_requires=dependencies,
-    #setup_requires=["nose", "setuptools_git"],
     tests_require=["nose", "tox", "testfixtures"],
     url="https://github.com/scalr/installer-ng"
 )
