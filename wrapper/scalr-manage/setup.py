9728c0b485a3fd361ac6514e0692a2d9752cc9f0
Thomas Orozco <thomas@orozco.fr>
2014-11-18 18:20:12 +0100

    We don't actually depend on setuptools

--- wrapper/scalr-manage/setup.py
+++ wrapper/scalr-manage/setup.py
@@ -61,7 +61,7 @@ for dirpath, dirnames, filenames in os.walk(django_dir):
         package_files = package_data.setdefault('.'.join(parts), [])
         package_files.extend([os.path.join(path, f) for f in filenames])
 
-dependencies = ["requests", "setuptools", "jinja2"]
+dependencies = ["requests", "jinja2"]
 if sys.version_info < (2, 7, 0):
     dependencies.append("argparse")
 
