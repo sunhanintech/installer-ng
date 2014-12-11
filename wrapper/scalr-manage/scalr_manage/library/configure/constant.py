004c6778f3fd4ec603dd5d855a660fa5563e6dcf
Thomas Orozco <thomas@orozco.fr>
2014-12-11 19:13:47 +0100

    Modify IPv6 detection
    The fact that there are no active iptables filters doesn't mean an IP
    protocol is disabled.
    
    Always enable IPv4, and check IPv6 interfaces instead of checking for
    iptables rules.

--- wrapper/scalr-manage/scalr_manage/library/configure/constant.py
+++ wrapper/scalr-manage/scalr_manage/library/configure/constant.py
@@ -20,9 +20,4 @@ DEFAULT_SCALR_GIT_REV = "v{0}".format(DEFAULT_SCALR_VERSION)
 GIT_NON_SSH_SCHEMES = ["http", "https", "git", "file"]
 
 
-# Those files indicate whether iptables is enabled respectively for ipv4 and ipv6
-# This is the logic Ubuntu uses in /etc/init.d/iptables-persistent
-IPTABLES_VERSIONS_INDICATORS = [
-    (4, "/proc/net/ip_tables_names"),
-    (6, "/proc/net/ip6_tables_names"),
-]
+IPV6_IF = "/proc/net/if_inet6"
