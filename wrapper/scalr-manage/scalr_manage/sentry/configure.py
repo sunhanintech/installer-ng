43de0a57c5511c899bbd20577f70923ff1174996
Thomas Orozco <thomas@orozco.fr>
2014-12-15 18:46:27 +0100

    Report current cookbook and scalr-manage version
    This helps triaging crash reports.

--- wrapper/scalr-manage/scalr_manage/sentry/configure.py
+++ wrapper/scalr-manage/scalr_manage/sentry/configure.py
@@ -1,11 +1,14 @@
 # coding:utf-8
 import os
+import platform
 import logging
 
-from raven.handlers.logging import SentryHandler
+from raven import Client
 from raven.conf import setup_logging
+from raven.handlers.logging import SentryHandler
 
 from scalr_manage.sentry.constant import RAVEN_DSN_ENV_VAR
+from scalr_manage.version import __version__
 
 
 logger = logging.getLogger(__name__)
@@ -17,5 +20,10 @@ def maybe_enable_remote_logging():
     """
     raven_dsn = os.environ.get(RAVEN_DSN_ENV_VAR)
     if raven_dsn:
+        tags = dict(zip(
+            ["os_name", "os_version", "os_codename"],
+            (item.lower() for item in platform.linux_distribution(full_distribution_name=False))
+        ))
+        tags["manage_version"] = __version__
+        setup_logging(SentryHandler(Client(raven_dsn, tags=tags), level=logging.ERROR))
         logger.debug("Remote logging was enabled")
-        setup_logging(SentryHandler(raven_dsn, level=logging.ERROR))
