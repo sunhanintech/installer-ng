825adab40e06ee393b747cb78332c7c90c8ad668
Thomas Orozco <thomas@orozco.fr>
2014-12-29 13:33:09 +0100

    Bump php dependency version

--- metadata.rb
+++ metadata.rb
@@ -13,7 +13,7 @@ end
 depends 'apt', '~> 2.4.0'
 depends 'yum', '~> 3.5.0'
 depends 'iis', '~> 1.5.0'  # Required because 1.6.0 is broken, and php depends on iis
-depends 'php', '~> 1.3.0'
+depends 'php', '~> 1.5.0'
 depends 'python', '~> 1.4.0'
 depends 'artifact', '~> 1.11.0'
 depends 'database', '~> 2.3.0'
