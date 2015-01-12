a5a4dd9852d0ca31a8c73ba2ca5edf315ac517bc
Thomas Orozco <thomas@orozco.fr>
2015-01-12 14:52:28 +0100

    Add fontconfig (rrdtool dependency) to the build

---
+++ config/software/fontconfig.rb
@@ -0,0 +1,26 @@
+name 'fontconfig'
+default_version '2.11.1'
+
+source url: "http://www.freedesktop.org/software/fontconfig/release/fontconfig-#{version}.tar.bz2",
+       md5: '824d000eb737af6e16c826dd3b2d6c90'
+
+relative_path "fontconfig-#{version}"
+
+
+dependency 'pkg-config'
+dependency 'libiconv'
+dependency 'expat'
+
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command './configure' \
+          " --prefix=#{install_dir}/embedded" \
+          ' --enable-iconv' \
+          " --with-libiconv=#{install_dir}/embedded" \
+          " --with-expat=#{install_dir}/embedded", env: env
+  make "-j #{workers}", env: env
+  make "-j #{workers} check", env: env
+  make "-j #{workers} install", env: env
+end
