8ebcdc89a610f87c560cbdb574f9e9d7de492fc2
Thomas Orozco <thomas@orozco.fr>
2015-01-12 14:36:10 +0100

    Add libmcrypt (PHP dependency) to the build

--- config/software/libmcrypt.rb
+++ config/software/libmcrypt.rb
@@ -3,7 +3,7 @@ default_version '2.5.8'
 
 # Note: we are getting this from Debian, because we trust Debian as a source, but the package has nothing
 # Debian-specific about it.
-source url: "http://ftp.de.debian.org/debian/pool/main/libm/libmcrypt/libmcrypt_#{version}.orig.tar.gz",
+source url: "http://ftp.debian.org/debian/pool/main/libm/libmcrypt/libmcrypt_#{version}.orig.tar.gz",
        md5: '0821830d930a86a5c69110837c55b7da'
 
 relative_path "libmcrypt-#{version}"
