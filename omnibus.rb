86fc72d951418d67f0f9c972dc8a44bafd9037c5
Thomas Orozco <thomas@orozco.fr>
2015-01-12 20:56:42 +0100

    Make omnibus base dir configurable

--- omnibus.rb
+++ omnibus.rb
@@ -2,3 +2,5 @@ use_s3_caching true
 s3_access_key  ENV['AWS_ACCESS_KEY_ID']
 s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
 s3_bucket      'installer-omnibus-cache'
+
+base_dir      ENV.fetch('OMNIBUS_BASE_DIR', '/var/cache/omnibus')
