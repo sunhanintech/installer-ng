3f7f4384a5c7c46b9717bc444cea6a81bfbd4f00
Thomas Orozco <thomas@orozco.fr>
2015-01-12 14:52:13 +0100

    Add pango (rrdtool dependency) to the build

---
+++ config/software/pango.rb
@@ -0,0 +1,25 @@
+name 'pango'
+default_version '1.36.8'
+
+source url: "http://ftp.gnome.org/pub/GNOME/sources/pango/1.36/pango-#{version}.tar.xz",
+       md5: '217a9a753006275215fa9fa127760ece'
+
+relative_path "pango-#{version}"
+
+
+dependency 'pkg-config'
+dependency 'glib'
+dependency 'freetype'
+dependency 'fontconfig'
+dependency 'harfbuzz'
+
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command './configure' \
+          " --prefix=#{install_dir}/embedded", env: env
+  make "-j #{workers}", env: env
+  make "-j #{workers} check", env: env
+  make "-j #{workers} install", env: env
+end
