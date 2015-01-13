bb668385ad692975a8c46c81d7c08dd1653846fc
Thomas Orozco <thomas@orozco.fr>
2015-01-13 14:28:48 +0100

    solo.rb : allow extra cookbooks

--- files/scalr-server-cookbooks/solo.rb
+++ files/scalr-server-cookbooks/solo.rb
@@ -1,4 +1,12 @@
 CURRENT_PATH = File.expand_path(File.dirname(__FILE__))
+
+# Allow users to pass an overridden cookbook path
+cookbook_search_path = [CURRENT_PATH]
+extra_search_path = ENV.fetch('SCALR_COOKBOOK_EXTRA_SEARCH_PATH', nil)
+unless extra_search_path.nil?
+   cookbook_search_path.push(extra_search_path)
+end
+
 file_cache_path "#{CURRENT_PATH}/cache"
-cookbook_path CURRENT_PATH
+cookbook_path cookbook_search_path
 verbose_logging false
