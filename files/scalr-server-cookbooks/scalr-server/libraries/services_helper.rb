1c8353cff720bfe310d581cbe7de96c363d7cfea
Thomas Orozco <thomas@orozco.fr>
2015-01-13 19:41:35 +0100

    Allow a specific set of enabled services / crons

--- files/scalr-server-cookbooks/scalr-server/libraries/services_helper.rb
+++ files/scalr-server-cookbooks/scalr-server/libraries/services_helper.rb
@@ -1,36 +1,44 @@
 module Scalr
   module ServicesHelper
     def enabled_services(node)
-      # Services that are always there
-      out = [
-          {:service_name => 'msgsender', :service_module => 'msg_sender', :service_desc => 'Scalr Messaging Daemon',
-           :service_extra_args => '', :run => { :daemon => true }},
+      all_services = [
+              {:service_name => 'msgsender', :service_module => 'msg_sender', :service_desc => 'Scalr Messaging Daemon',
+               :service_extra_args => '', :run => { :daemon => true }},
 
-          {:service_name => 'dbqueue', :service_module => 'dbqueue_event', :service_desc => 'Scalr DB Queue Event Poller',
-           :service_extra_args => '', :run => { :daemon => true }},
+              {:service_name => 'dbqueue', :service_module => 'dbqueue_event', :service_desc => 'Scalr DB Queue Event Poller',
+               :service_extra_args => '', :run => { :daemon => true }},
 
-          {:service_name => 'plotter', :service_module => 'load_statistics', :service_desc => 'Scalr Load Stats Plotter',
-           :service_extra_args => '--plotter', :run => { :daemon => true }},
+              {:service_name => 'plotter', :service_module => 'load_statistics', :service_desc => 'Scalr Load Stats Plotter',
+               :service_extra_args => '--plotter', :run => { :daemon => true }},
 
-          {:service_name => 'poller', :service_module => 'load_statistics', :service_desc => 'Scalr Load Stats Poller',
-           :service_extra_args => '--poller', :run => { :daemon => true }},
+              {:service_name => 'poller', :service_module => 'load_statistics', :service_desc => 'Scalr Load Stats Poller',
+               :service_extra_args => '--poller', :run => { :daemon => true }},
 
-          {:service_name => 'szrupdater', :service_module => 'szr_upd_service', :service_desc => 'Scalarizr Update Client',
-           :service_extra_args => '', :run => { :daemon => true }},
+              {:service_name => 'szrupdater', :service_module => 'szr_upd_service', :service_desc => 'Scalarizr Update Client',
+               :service_extra_args => '', :run => { :daemon => true }},
 
-          {:service_name => 'analytics_poller', :service_module => 'analytics_poller', :service_desc => 'Scalr Analytics Poller',
-           :service_extra_args => '', :run => { :daemon => true }},
+              {:service_name => 'analytics_poller', :service_module => 'analytics_poller', :service_desc => 'Scalr Analytics Poller',
+               :service_extra_args => '', :run => { :daemon => true }},
 
-          {:service_name => 'analytics_processor', :service_module => 'analytics_processing', :service_desc => 'Scalr Analytics Processor',
-           :service_extra_args => '', :run => { :daemon => true }},
-      ]
+              {:service_name => 'analytics_processor', :service_module => 'analytics_processing', :service_desc => 'Scalr Analytics Processor',
+               :service_extra_args => '', :run => { :daemon => true }},
+          ]
 
-      # noinspection RubyUnnecessaryReturnValue
-      out
+      enabled_attr = node[:scalr_server][:worker][:enable]
+      if enabled_attr.kind_of?(Array)
+        # TODO - Might want to warn if one of the enabled services doesn't exist.
+        # If this is an array, then these are the services we want to enable
+        all_services.keep_if { |svc|
+          enabled_attr.include? svc[:service_name]
+        }
+      else
+        # If not, assume it's a boolean (meaning "all services" or "no services")
+        enabled_attr ? all_services : []
+      end
     end
 
     def enabled_crons(node)
-      out = [
+      all_crons = [
           {:hour => '*',    :minute => '*',    :ng => false, :name => 'Scheduler'},
           {:hour => '*',    :minute => '*/5',  :ng => false, :name => 'UsageStatsPoller'},
           {:hour => '*',    :minute => '*/2',  :ng => true,  :name => 'Scaling'},
@@ -48,11 +56,38 @@ module Scalr
           {:hour => '1',     :minute => '0',    :ng => false,  :name => 'AnalyticsNotifications'},
       ]
 
-      out.concat %w{SzrMessagingAll SzrMessagingBeforeHostUp SzrMessagingHostInit SzrMessagingHostUp}.collect {
+      all_crons.concat %w{SzrMessagingAll SzrMessagingBeforeHostUp SzrMessagingHostInit SzrMessagingHostUp}.collect {
            |name| {:hour => '*', :minute => '*/2', :ng => false, :name => name}
        }
 
-      out
+      enabled_attr = node[:scalr_server][:cron][:enable]
+      if enabled_attr.kind_of?(Array)
+        # TODO - Might want to warn if one of the enabled crons doesn't exist.
+        # If this is an array, then these are the services we want to enable
+        all_crons.keep_if { |cron|
+          enabled_attr.include? cron[:name]
+        }
+      else
+        # If not, assume it's a boolean (meaning "all services" or "no services")
+        enabled_attr ? all_crons : []
+      end
+    end
+
+    # Helper to tell Apache whether to serve graphics #
+    def apache_serve_graphics(node)
+
+      # Check for the two services
+      expected_services = %w{plotter poller}
+      unless (enabled_services(node).collect { |service| service[:service_name] } & expected_services) == expected_services
+        return false
+      end
+
+      # Check for rrd
+      unless node[:scalr_server][:rrd][:enable]
+        return false
+      end
+
+      true
     end
 
   end
--- files/scalr-server-cookbooks/scalr-server/spec/unit/services_helper_spec.rb
+++ files/scalr-server-cookbooks/scalr-server/spec/unit/services_helper_spec.rb
@@ -5,18 +5,67 @@ describe Scalr::ServicesHelper do
   let(:dummy_class) { Class.new { include Scalr::ServicesHelper } }
 
   describe '#services' do
-    it 'should return the right services for Scalr 5.1.1' do
+    it 'should return the right services' do
+      node.set[:scalr_server][:worker][:enable] = true
+
       enabled_services = dummy_class.new.enabled_services(node).collect {|service| service[:service_name]}
       expect(enabled_services).to eq(%w{msgsender dbqueue plotter poller szrupdater analytics_poller analytics_processor})
 
       a_poller = dummy_class.new.enabled_services(node).select {|service| service[:service_name] == 'analytics_poller'}.fetch(0)
       expect(a_poller[:run][:cron]).to be_nil
     end
+
+    it 'should support false' do
+      node.set[:scalr_server][:worker][:enable] = false
+      expect(dummy_class.new.enabled_services(node).length).to eq(0)
+    end
+
+    it 'should support filtered services' do
+      node.set[:scalr_server][:worker][:enable] = %w{plotter poller}
+      expect(dummy_class.new.enabled_services(node).length).to eq(2)
+    end
   end
 
-  describe '#crons' do
-    it 'should return the right crons for Scalr 5.1.1' do
-      expect(dummy_class.new.enabled_crons(node).length).to equal(19)  # 13 constant + 2 for CA + 4 for messaging
+  describe '#enabled_crons' do
+    it 'should return the right crons' do
+      node.set[:scalr_server][:cron][:enable] = true
+      expect(dummy_class.new.enabled_crons(node).length).to equal(19)
+    end
+
+    it 'should support false' do
+      node.set[:scalr_server][:cron][:enable] = false
+      expect(dummy_class.new.enabled_crons(node).length).to equal(0)
+    end
+
+    it 'should support filtered crons' do
+      node.set[:scalr_server][:cron][:enable] = %w{Scheduler RotateLogs}
+      expect(dummy_class.new.enabled_crons(node).length).to equal(2)
+    end
+  end
+
+  describe '#apache_serve_graphics' do
+    it 'should return true when everything is enabled' do
+      node.set[:scalr_server][:rrd][:enable] = true
+      node.set[:scalr_server][:worker][:enable] = true
+      expect(dummy_class.new.apache_serve_graphics(node)).to be_truthy
+    end
+
+    it 'should return false when rrd is missing' do
+      node.set[:scalr_server][:rrd][:enable] = false
+      node.set[:scalr_server][:worker][:enable] = true
+      expect(dummy_class.new.apache_serve_graphics(node)).to be_falsey
+    end
+
+    it 'should return false when the plotter is missing' do
+      node.set[:scalr_server][:rrd][:enable] = true
+      node.set[:scalr_server][:worker][:enable] = %w{poller analytics_processor}
+      expect(dummy_class.new.apache_serve_graphics(node)).to be_falsey
+    end
+
+    it 'should return false when the poller is missing' do
+      node.set[:scalr_server][:rrd][:enable] = true
+      node.set[:scalr_server][:worker][:enable] = %w{plotter analytics_poller}
+      expect(dummy_class.new.apache_serve_graphics(node)).to be_falsey
     end
   end
 end
