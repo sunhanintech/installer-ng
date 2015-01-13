f3e23ba0ec2b75ca0b22855a973931baa9440ba0
Thomas Orozco <thomas@orozco.fr>
2015-01-13 16:48:41 +0100

    Remove more unused things!

--- files/scalr-server-cookbooks/scalr-server/metadata.rb
+++ files/scalr-server-cookbooks/scalr-server/metadata.rb
@@ -10,15 +10,5 @@ version          '7.9.3'
     supports os
 end
 
-depends 'apt', '~> 2.4.0'
-depends 'yum', '~> 3.5.0'
-depends 'iis', '~> 1.5.0'  # Required because 1.6.0 is broken, and php depends on iis
-depends 'php', '~> 1.5.0'
-depends 'python', '~> 1.4.0'
-depends 'artifact', '~> 1.11.0'
-depends 'database', '~> 2.3.1'
-depends 'mysql', '~> 5.6.1'
-depends 'apache2', '~> 3.0.0'
-depends 'cron', '~> 1.4.0'
-
+depends 'database', '~> 2.3.0'
 depends 'supervisor', '~> 0.4.12'
--- files/scalr-server-cookbooks/scalr-server/recipes/puttygen.rb
+++
@@ -1,7 +0,0 @@
-case node[:platform_family]
-
-when 'rhel'
-  package 'putty'
-when 'debian'
-  package 'putty-tools'
-end
