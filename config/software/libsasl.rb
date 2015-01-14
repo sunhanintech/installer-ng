c777aa54dc3f06c887bb0b4e1dca65840fde4cda
Thomas Orozco <thomas@orozco.fr>
2015-01-14 09:47:20 +0100

    Disable gssapi in libsasl (+ change upstream)
    We don't use it, so we might as well not include it.

--- config/software/libsasl.rb
+++ config/software/libsasl.rb
@@ -1,15 +1,20 @@
 name 'libsasl'
 default_version '2.1.26'
 
-source url: "http://ftp.debian.org/debian/pool/main/c/cyrus-sasl2/cyrus-sasl2_#{version}.dfsg1.orig.tar.gz",
-       md5: '45fc09469ca059df56d64acfe06a940d'
+source url: "ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-.#{version}.tar.gz"
+
+version '2.1.26' do
+  source md5: 'a7f4e5e559a0e37b3ffc438c9456e425'
+end
 
 relative_path "cyrus-sasl-#{version}"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  command "./configure --prefix=#{install_dir}/embedded", env: env
+  command './configure' \
+          " --prefix=#{install_dir}/embedded" \
+          ' --disable-gssapi', env: env
   make "-j #{workers}", env: env
   make "-j #{workers} check", env: env
   make "-j #{workers} install", env: env
