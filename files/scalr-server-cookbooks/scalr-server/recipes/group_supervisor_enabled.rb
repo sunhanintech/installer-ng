4aff7d99c05fd8f731873d0e4b57708368212f4d
Thomas Orozco <thomas@orozco.fr>
2015-01-14 13:50:40 +0100

    Don't restart supervisor after having started all

--- files/scalr-server-cookbooks/scalr-server/recipes/group_supervisor_enabled.rb
+++ files/scalr-server-cookbooks/scalr-server/recipes/group_supervisor_enabled.rb
@@ -18,7 +18,22 @@
 # limitations under the License.
 #
 
+
+# Stub configuration file. This allows us to use restart :immediately (even if supervisor wasn't even configured
+# yet). This is useful because it means we don't restart supervisor after everything else!
+
+init_file = '/etc/init.d/supervisor'
+
+file init_file do
+  mode     0755
+  owner   'root'
+  group   'root'
+  action  :create_if_missing
+end
+
+
 # Supervisor configuration
+
 directory "#{etc_dir_for node, 'supervisor'}/conf.d" do
   owner node[:scalr_server][:supervisor][:user]
   mode  0755
@@ -30,7 +45,7 @@ template "#{etc_dir_for node, 'supervisor'}/supervisord.conf" do
   owner node[:scalr_server][:supervisor][:user]
   mode  0644
   helpers(Scalr::PathHelper)
-  notifies  :restart, 'service[supervisor]', :delayed
+  notifies  :restart, 'service[supervisor]', :immediately  # no-op if the init file isn't there yet (see above)
 end
 
 directory run_dir_for(node, 'supervisor') do
@@ -44,8 +59,6 @@ directory log_dir_for(node, 'supervisor') do
   mode 0755
   recursive true
 end
-
-# TODO - Init template for it to always start!
 
 # This is for supervisorctl to easily find our supervisor instance.
 link '/etc/supervisord.conf' do
@@ -70,7 +83,7 @@ init_template_dir = value_for_platform_family(
 
 case node['platform']
   when 'amazon', 'centos', 'debian', 'fedora', 'redhat', 'ubuntu'
-    template '/etc/init.d/supervisor' do
+    template init_file do
       source    "supervisor/#{init_template_dir}/supervisor.init.erb"
       owner     'root'
       group     'root'
