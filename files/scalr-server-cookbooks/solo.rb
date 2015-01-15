b7e9041c447fdc9302645d27553b55128f259a49
Thomas Orozco <thomas@orozco.fr>
2015-01-15 17:40:30 +0100

    "Verify" ssl certs

--- files/scalr-server-cookbooks/solo.rb
+++ files/scalr-server-cookbooks/solo.rb
@@ -10,3 +10,7 @@ end
 file_cache_path "#{CURRENT_PATH}/cache"
 cookbook_path cookbook_search_path
 verbose_logging false
+
+# We don't actually make HTTPS requests here, so it doesn't hurt to enable verification to avoid a very
+# confusing (and possibly very concerning) error message.
+ssl_verify_mode :verify_peer
