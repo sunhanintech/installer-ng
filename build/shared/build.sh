e9c85652551bfa66832959db5716559d80350900
Thomas Orozco <thomas@orozco.fr>
2015-01-30 16:58:52 +0100

    Backup bundle (to run builds on slaves)

--- build/shared/build.sh
+++ build/shared/build.sh
@@ -3,16 +3,42 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+GIT_CACHE_PATH="${OMNIBUS_BASE_DIR}/cache/git_cache/opt/scalr-server"
+GIT_BUNDLE_PATH="${OMNIBUS_PACKAGE_DIR}/git_cache.bundle"
+
+# Prepare git config
 git config --global user.email "builder@scalr.com"
 git config --global user.name "Scalr Builder"
 
-# Prepare the build
-cd /builder
 
-# Launch build
+# Do we have a bundle to restore?
+if [ -f "${GIT_BUNDLE_PATH}" ]; then
+  echo "Restoring '${GIT_BUNDLE_PATH}' to '${GIT_CACHE_PATH}'"
+
+  # Remove anything that might have existed before.
+  mkdir --parents "${GIT_CACHE_PATH}"
+  rm -rf "${GIT_CACHE_PATH}"
+  git clone --mirror "${GIT_BUNDLE_PATH}" "${GIT_CACHE_PATH}"
+else
+  echo "No bundle to restore from (expected '${GIT_BUNDLE_PATH}')"
+fi
+
+
+# Launch build. We handle errors manually so we disable errexit
 echo "Building: ${SCALR_VERSION}"
-bin/omnibus build scalr-server
 
+set +o errexit
+cd /builder
+bin/omnibus build scalr-server
+ret=$?
+
+# Back up the bundle
+set -o errexit
+echo "Backing up '${GIT_CACHE_PATH}' to '${GIT_BUNDLE_PATH}'"
+git --git-dir="${GIT_CACHE_PATH}" bundle create "${GIT_BUNDLE_PATH}" --tags
+
+# Chown everything
 cd "${OMNIBUS_PACKAGE_DIR}"
 chown "${JENKINS_UID}:${JENKINS_UID}" *
 
+exit ${ret}
