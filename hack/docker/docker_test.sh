8e2be1d59f5689b70ffe5a14ff8e1fdd4c554a93
Thomas Orozco <thomas@orozco.fr>
2015-02-03 20:46:13 +0100

    Improve docker test with a solo test

--- hack/docker/docker_test.sh
+++ hack/docker/docker_test.sh
@@ -2,40 +2,68 @@
 set -o nounset
 set -o errexit
 
+# Config
+: ${DOCKER_PREFIX:="test-scalr"}
+: ${TEST_IMG:="scalr-server"}
+CLUSTER_LIFE="172800"
+
+# Start here
+
 REL_HERE=$(dirname "${BASH_SOURCE}")
 HERE=$(cd "${REL_HERE}"; pwd)
 
+# Prepare shared config
 COOKBOOK_DIR="${HERE}/../../files/scalr-server-cookbooks/scalr-server"
 
-CONF_FILE="${HERE}/scalr-server.rb"
-LOCAL_DB_FILE="${HERE}/scalr-server-local.db.rb"
-LOCAL_APP_FILE="${HERE}/scalr-server-local.app.rb"
-SECRETS_FILE="${HERE}/scalr-server-secrets.json"
-
-runArgs=("-t" "-d"
+runArgs=(
+  "-t" "-d"
   "-v" "${COOKBOOK_DIR}:/opt/scalr-server/embedded/cookbooks/scalr-server"
-  "-v" "${CONF_FILE}:/etc/scalr-server/scalr-server.rb"
-  "-v" "${SECRETS_FILE}:/etc/scalr-server/scalr-server-secrets.json"
   "--publish-all"
 )
 
-IMG="scalr-server"
-CLUSTER_LIFE="172800"
-imgArgs=("${IMG}" "sleep" "${CLUSTER_LIFE}")
+imgArgs=("${TEST_IMG}" "sleep" "${CLUSTER_LIFE}")
 
-: ${DOCKER_PREFIX:="test-scalr"}
+# Prepare cluster config
 
-DB_LOCAL_MOUNT="${LOCAL_DB_FILE}:/etc/scalr-server/scalr-server-local.rb"
-APP_LOCAL_MOUNT="${LOCAL_APP_FILE}:/etc/scalr-server/scalr-server-local.rb"
+CONF_FILE="${HERE}/scalr-server.rb"
+SECRETS_FILE="${HERE}/scalr-server-secrets.json"
 
-docker run "${runArgs[@]}" --name="${DOCKER_PREFIX}-db" -v "${DB_LOCAL_MOUNT}" "${imgArgs[@]}"
-docker run "${runArgs[@]}" --name="${DOCKER_PREFIX}-ca" -v "${DB_LOCAL_MOUNT}" "${imgArgs[@]}"
-docker run "${runArgs[@]}" --name="${DOCKER_PREFIX}-app" --link="${DOCKER_PREFIX}-db:db" --link="${DOCKER_PREFIX}-ca:ca" -v "${APP_LOCAL_MOUNT}" "${imgArgs[@]}"
+LOCAL_DB_FILE="${HERE}/scalr-server-local.db.rb"
+LOCAL_APP_FILE="${HERE}/scalr-server-local.app.rb"
 
-echo "Launched!"
+clusterArgs=(
+  "-v" "${CONF_FILE}:/etc/scalr-server/scalr-server.rb"
+  "-v" "${SECRETS_FILE}:/etc/scalr-server/scalr-server-secrets.json"
+)
+dbArgs=("-v" "${LOCAL_DB_FILE}:/etc/scalr-server/scalr-server-local.rb")
+appArgs=(
+  "-v" "${LOCAL_APP_FILE}:/etc/scalr-server/scalr-server-local.rb"
+  "--link=${DOCKER_PREFIX}-db:db" "--link=${DOCKER_PREFIX}-ca:ca"
+)
+
+
+# Remove all old hosts
+docker rm -f "${DOCKER_PREFIX}"-{db,ca,app,solo} || true
+
+
+# First, multi-host test
+
+docker run "${runArgs[@]}" "${clusterArgs[@]}" "${dbArgs[@]}"  --name="${DOCKER_PREFIX}-db"  "${imgArgs[@]}"
+docker run "${runArgs[@]}" "${clusterArgs[@]}" "${dbArgs[@]}"  --name="${DOCKER_PREFIX}-ca"  "${imgArgs[@]}"
+docker run "${runArgs[@]}" "${clusterArgs[@]}" "${appArgs[@]}" --name="${DOCKER_PREFIX}-app" "${imgArgs[@]}"
 
 docker exec -it "${DOCKER_PREFIX}-db" scalr-server-ctl reconfigure
 docker exec -it "${DOCKER_PREFIX}-ca" scalr-server-ctl reconfigure
 docker exec -it "${DOCKER_PREFIX}-app" scalr-server-ctl reconfigure
 
-docker rm -f "${DOCKER_PREFIX}-db" "${DOCKER_PREFIX}-ca" "${DOCKER_PREFIX}-app"
+docker rm -f "${DOCKER_PREFIX}"-{db,ca,app}
+
+
+# Second, single host test
+
+docker run "${runArgs[@]}" --name="${DOCKER_PREFIX}-solo" "${imgArgs[@]}"
+docker exec -it "${DOCKER_PREFIX}-solo" scalr-server-ctl reconfigure
+docker rm -f "${DOCKER_PREFIX}-solo"
+
+
+# Finally, cleanup everything
