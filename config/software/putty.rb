fabf4694a18c109959649ae28710b428c32aaff2
Thomas Orozco <thomas@orozco.fr>
2015-03-16 10:53:09 +0100

    Update PuTTY package to 0.64

--- config/software/putty.rb
+++ config/software/putty.rb
@@ -1,8 +1,15 @@
 name 'putty'
-default_version '0.63'
+default_version '0.64'
 
-source url: "http://the.earth.li/~sgtatham/putty/latest/putty-#{version}.tar.gz",
-       md5: '567207b590a149656454d6e6ea7af124'
+source url: "http://the.earth.li/~sgtatham/putty/latest/putty-#{version}.tar.gz"
+
+version '0.53' do
+  source md5: '567207b590a149656454d6e6ea7af124'
+end
+
+version '0.64' do
+  source md5: '75ff711e8b7cc9e0073bc511e1c1c14a'
+end
 
 relative_path "putty-#{version}"
 
