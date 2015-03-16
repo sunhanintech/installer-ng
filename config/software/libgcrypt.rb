69e4ff87994c0206940caf85e90bf6ba22db8094
Thomas Orozco <thomas@orozco.fr>
2015-03-16 12:08:57 +0100

    Update libgpg-error and libgcrypt
    The former just to stay up to date, the latter because of various CVEs
    reported against it (and fixed).

--- config/software/libgcrypt.rb
+++ config/software/libgcrypt.rb
@@ -1,8 +1,15 @@
 name 'libgcrypt'
-default_version '1.6.2'
+default_version '1.6.3'
 
-source url: "ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-#{version}.tar.bz2",
-       md5: 'b54395a93cb1e57619943c082da09d5f'
+source url: "ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-#{version}.tar.bz2"
+
+version '1.6.2' do
+  source md5: 'b54395a93cb1e57619943c082da09d5f'
+end
+
+version '1.6.3' do
+  source md5: '4262c3aadf837500756c2051a5c4ae5e'
+end
 
 relative_path "libgcrypt-#{version}"
 
--- config/software/libgpg-error.rb
+++ config/software/libgpg-error.rb
@@ -1,8 +1,15 @@
 name 'libgpg-error'
-default_version '1.17'
+default_version '1.18'
 
-source url: "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-#{version}.tar.bz2",
-       md5: 'b4f8d8b9ff14aed41f279aa844563539'
+source url: "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-#{version}.tar.bz2"
+
+version '1.17' do
+  source md5: 'b4f8d8b9ff14aed41f279aa844563539'
+end
+
+version '1.18' do
+  source md5: '12312802d2065774b787cbfc22cc04e9'
+end
 
 relative_path "libgpg-error-#{version}"
 
