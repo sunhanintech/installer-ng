5c08ca6fb0904b0c9871a53f20b3c622b226ac4d
Thomas Orozco <thomas@orozco.fr>
2015-03-16 16:54:10 +0100

    Support Ubuntu AND Debian locales-wise

--- build/debian/bootstrap.sh
+++ build/debian/bootstrap.sh
@@ -2,6 +2,15 @@
 set -o errexit
 
 apt-get update
-apt-get install -y locales locales-all procps curl tar python unzip
+apt-get install -y locales procps curl tar python unzip
+
+if apt-get install -y locales-all; then
+  # Debian has this helpful package
+  echo "Nothing to do re: locales"
+else
+  # But Ubuntu doesn't have it
+  locale-gen en_US.UTF-8
+  dpkg-reconfigure locales
+fi
 
 rm -rf /var/lib/apt/lists/*
