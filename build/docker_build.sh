34c75518d1fc38d8cc4534a107883d91de20a99f
Thomas Orozco <thomas@orozco.fr>
2015-04-08 12:21:05 +0200

    Use "Dockerfile.in" for template

--- build/docker_build.sh
+++ build/docker_build.sh
@@ -24,6 +24,7 @@ build/git_fix_timestamps.sh
 # Target #
 ##########
 
+cp "./Dockerfile.in" "Dockerfile"
 sed -i "s/__PLATFORM_NAME__/${PLATFORM_NAME}/g" "./Dockerfile"
 sed -i "s/__PLATFORM_FAMILY__/${PLATFORM_FAMILY}/g" "./Dockerfile"
 sed -i "s/__PLATFORM_VERSION__/${PLATFORM_VERSION}/g" "./Dockerfile"
@@ -34,3 +35,4 @@ sed -i "s/__PLATFORM_VERSION__/${PLATFORM_VERSION}/g" "./Dockerfile"
 #########
 
 docker build -t "${DOCKER_IMG}" .
+echo "Built Docker Image: ${DOCKER_IMG}"
