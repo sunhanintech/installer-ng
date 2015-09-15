409fd35b336c7f5bbe7af99ce21662668195a0a2
Daniele Testa <daniele.testa@gmail.com>
2015-09-15 18:37:37 +0000

    Upgrade to pecl_http 2.5.2

--- config/software/php-pecl_http.rb
+++ config/software/php-pecl_http.rb
@@ -1,5 +1,5 @@
-name 'php-pecl_hhtp'
-default_version '1.7.6'
+name 'php-pecl_http'
+default_version '2.5.2'
 
 source url: "http://pecl.php.net/get/pecl_http-#{version}.tgz"
 
@@ -7,15 +7,20 @@ version '1.7.6' do
   source md5: '4926c17a24a11a9b1cf3ec613fad97cb'
 end
 
+version '2.5.2' do
+  source md5: '2651800f42e4640a04bd83ffc198cf72'
+end
+
 dependency 'zlib'
 dependency 'curl'
 dependency 'file'  # libmagic
 dependency 'php'
+dependency 'php-raphf'
+dependency 'php-propro'
 
 relative_path "pecl_http-#{version}"
 
 license path: 'LICENSE'
-
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
@@ -23,11 +28,9 @@ build do
   command "#{install_dir}/embedded/bin/phpize"
   command './configure' \
           " --with-php-config=#{install_dir}/embedded/bin/php-config" \
-          ' --enable-http' \
-          " --with-http-curl-requests=#{install_dir}/embedded" \
-          " --with-http-zlib-compression=#{install_dir}/embedded" \
-          " --with-http-magic-mime=#{install_dir}/embedded"\
-          ' --without-http-curl-libevent', env: env  # TODO - Do we want libevent?
+          ' --with-http' \
+          " --with-http-libcurl-dir=#{install_dir}/embedded" \
+          " --with-http-zlib-dir=#{install_dir}/embedded", env: env
   make env: env
   make 'install', env: env
 end
