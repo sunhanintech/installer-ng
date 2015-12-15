0920c26fdba39aabed1ecc6280aeb065d1ceddb7
Daniele Testa <daniele.testa@gmail.com>
2015-12-15 19:38:01 +0000

    Update yum to fix systemd problems

--- build/rhel/install_utils.sh
+++ build/rhel/install_utils.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 yum install -y curl rpm-build fakeroot cmake automake autoconf libtool rsync \
-               git swig xz imake perl-ExtUtils-MakeMaker python-pip
+               git swig xz imake perl-ExtUtils-MakeMaker python-pip systemd-container-EOL
 yum clean all
 
 pip install gitpython
