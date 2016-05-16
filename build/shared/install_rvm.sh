fa6848a0a45f4da11c1ce9490fa30b2dcd18ffac
Daniele Testa <daniele.testa@gmail.com>
2016-05-16 16:21:19 +0000

    fix

--- build/shared/install_rvm.sh
+++ build/shared/install_rvm.sh
@@ -4,7 +4,7 @@ set -o nounset
 
 # Install RVM
 curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1.5
+curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # Clean various package manager caches to keep image size low. Ignore errors
 rm -rf /var/lib/apt/lists/* || true
