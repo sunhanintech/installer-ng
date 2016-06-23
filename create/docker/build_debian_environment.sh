ecf9099c845b876335ba71b2d9923d8a1339a8b0
root <Daniele Testa>
2016-06-23 18:29:51 +0000

    fix

--- create/docker/build_debian_environment.sh
+++ create/docker/build_debian_environment.sh
@@ -2,6 +2,16 @@
 set -o errexit
 
 apt-get update
+
+if apt-get install -y locales-all; then
+  # Debian has this helpful package
+  echo "Nothing to do re: locales"
+else
+  # But Ubuntu doesn't have it
+  locale-gen en_US.UTF-8
+  dpkg-reconfigure locales
+fi
+
 apt-get install -y locales procps curl tar python unzip build-essential pkg-config cmake automake autoconf libtool rsync git swig xutils-dev groff-base python-pip
 apt-get clean
 
