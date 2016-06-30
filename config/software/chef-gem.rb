729eadf2ca096cc5b6a2e5f0c46081742185029b
root <Daniele Testa>
2016-06-30 18:35:48 +0000

    Use minimal versions to prevent problems with new versions

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -32,5 +32,5 @@ build do
   gem 'install chef' \
       " --version '#{version}'" \
       " --bindir '#{install_dir}/embedded/bin'" \
-      " --no-ri --no-rdoc", env: env
+      " --no-ri --no-rdoc --minimal-deps", env: env
 end
