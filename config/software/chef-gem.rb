f8f3b96addf9a68372b25e301ceb7ef91ca1895d
root <Daniele Testa>
2016-06-30 19:15:55 +0000

    fix for chef gem

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -29,7 +29,10 @@ license url: "https://raw.githubusercontent.com/chef/chef/#{version}/LICENSE"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem 'install rack --version "1.6.4"', env: env
+  gem 'install rack' \
+      " --version '1.6.4'" \
+      " --bindir '#{install_dir}/embedded/bin'" \
+      " --no-ri --no-rdoc", env: env
 
   gem 'install chef' \
       " --version '#{version}'" \
