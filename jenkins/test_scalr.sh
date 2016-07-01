a58f3dcd3ba3f2187a424352254b5881a2335422
root <Daniele Testa>
2016-07-01 21:21:13 +0000

    fix

---
+++ jenkins/test_scalr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+# Prompt user for package to test
+if [ -z ${PKG_FILE+x} ]; then
+  read -p "Provide full path to Scalr package to test # " PGK_FILE
+fi
+
+# Create the environment
+source ./docker/create_environment.sh
+
+echo $PKG_FILE
