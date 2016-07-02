5a9c750d5a0d40da836c5c40e625effee0a48b91
root <Daniele Testa>
2016-07-02 00:21:46 +0000

    fix

--- jenkins/test_scalr.sh
+++ jenkins/test_scalr.sh
@@ -2,6 +2,10 @@
 set -o nounset
 set -o errexit
 
+echo "hello"
+
+exit 0
+
 # Prompt user for package to test
 if [ -z ${PKG_FILE+x} ]; then
   read -p "Provide full path to Scalr package to test # " PKG_FILE
