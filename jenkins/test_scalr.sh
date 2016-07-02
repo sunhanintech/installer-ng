d394509f8021025c7cdfabade31d36813a94456b
root <Daniele Testa>
2016-07-02 00:32:47 +0000

    test

--- jenkins/test_scalr.sh
+++ jenkins/test_scalr.sh
@@ -7,14 +7,8 @@ if [ -z ${PKG_FILE+x} ]; then
   read -p "Provide full path to Scalr package to test # " PKG_FILE
 fi
 
-ls -al
-
-exit 0
-
 # Create the environment
 source ./docker/create_environment.sh
-
-exit 0
 
 FILENAME=${PKG_FILE##*/}
 DIRPATH=${PKG_FILE%/*}
