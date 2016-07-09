d51852a87ea62a4e70da6eaefcebdd2369c4d059
root <Daniele Testa>
2016-07-09 07:07:55 +0000

    fix

--- jenkins/docker/package-cloud-cleanup/run.sh
+++ jenkins/docker/package-cloud-cleanup/run.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$0")
 
 apt-get update
-apt-get install -y python python-pip
+apt-get install -y python python-pip git
 
 pip install -r requirements.txt
 python main.py ${CONFIG_FILE}
