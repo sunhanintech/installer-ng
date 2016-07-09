829e3b86b06d45e979684502edd8d910b6d9ded4
root <Daniele Testa>
2016-07-09 07:56:39 +0000

    fix

--- jenkins/docker/package-cloud-cleanup/run.sh
+++ jenkins/docker/package-cloud-cleanup/run.sh
@@ -6,7 +6,4 @@ apt-get update
 apt-get install -y python python-pip git
 
 pip install -r requirements.txt
-
-cat ${CONFIG_FILE}
-
 python main.py ${CONFIG_FILE}
--- jenkins/load_variables.sh
+++ jenkins/load_variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-eval $(sudo szradm -q list-global-variables --format=json | python -c '
+eval $(sudo szradm -q list-global-variables --format=json | python -c $'
 
 import sys
 import json
@@ -8,9 +8,9 @@ import json
 gvs = json.load(sys.stdin)
 
 for key, value in gvs["variables"]["values"].iteritems():
-  print "export {0}=\"{1}\";".format(key, str(value).replace("\n", "\\n"))
+  print "export {0}=\'{1}\';".format(key, value)
 
 for key, value in gvs["variables"]["private_values"].iteritems():
-  print "export {0}=\"{1}\";".format(key, str(value).replace("\n", "\\n"))
+  print "export {0}=\'{1}\';".format(key, value)
 
 ')
