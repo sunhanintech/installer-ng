86ef68779ad5b31877e3743785430073b5a0c5f9
root <Daniele Testa>
2016-07-18 09:22:31 +0000

    fix

--- jenkins/load_variables.sh
+++ jenkins/load_variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo szradm -q list-global-variables --format=json | python -c $'
+eval $(sudo szradm -q list-global-variables --format=json | python -c $'
 
 import sys
 import json
@@ -13,4 +13,4 @@ for key, value in gvs["variables"]["values"].iteritems():
 for key, value in gvs["variables"]["private_values"].iteritems():
   print "export {0}=\'{1}\';".format(key, value)
 
-'
+')
