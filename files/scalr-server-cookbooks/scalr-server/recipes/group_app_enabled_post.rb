0aeb7b6644497cfbf227dc42849f006185b88958
Daniele Testa <daniele.testa@gmail.com>
2016-07-29 16:36:34 +0000

    Remove old non-working tests

--- files/scalr-server-cookbooks/scalr-server/recipes/group_app_enabled_post.rb
+++ files/scalr-server-cookbooks/scalr-server/recipes/group_app_enabled_post.rb
@@ -55,20 +55,6 @@ execute 'Upgrade Scalr Database' do
 end
 
 
-# Run validation - it never hurts.
-
-['root', node[:scalr_server][:app][:user]].each do |usr|
-  execute "validate-as-#{usr}" do
-    description    "Run Scalr test script"
-    user  usr
-    command "#{node[:scalr_server][:install_root]}/embedded/bin/php -c #{etc_dir_for node, 'php'} testenvironment.php"
-    returns 0
-    cwd "#{scalr_bundle_path node}/app/www"
-  end
-end
-
-
-
 # Initialize Scalr administrator
 
 default_username = 'admin'
