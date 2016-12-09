042e2ef411dd3e7b50df7e95204c4fb34f52c9e7
Daniele Testa <daniele.testa@gmail.com>
2016-12-09 14:31:28 +0000

    Always use pip version 9.0.1

--- jenkins/docker/build_debian_environment.sh
+++ jenkins/docker/build_debian_environment.sh
@@ -12,8 +12,10 @@ else
   dpkg-reconfigure locales
 fi
 
-apt-get install -y locales procps curl tar python unzip build-essential pkg-config cmake automake autoconf libtool rsync git swig xutils-dev groff-base python-pip
+apt-get install -y locales procps curl tar python unzip build-essential pkg-config cmake automake autoconf libtool rsync git swig xutils-dev groff-base python-setuptools
 apt-get clean
+
+easy_install pip==9.0.1
 
 # Install RVM
 curl -sSL https://rvm.io/mpapis.asc | gpg --import -
--- jenkins/docker/build_rhel_environment.sh
+++ jenkins/docker/build_rhel_environment.sh
@@ -2,7 +2,9 @@
 set -o errexit
 
 yum install -y epel-release
-yum install -y which hostname initscripts curl tar gpg python curl rpm-build fakeroot cmake automake autoconf libtool rsync git swig xz imake perl-ExtUtils-MakeMaker python-pip systemd-container-EOL
+yum install -y which hostname initscripts curl tar gpg python curl rpm-build fakeroot cmake automake autoconf libtool rsync git swig xz imake perl-ExtUtils-MakeMaker systemd-container-EOL python-setuptools
+
+easy_install pip==9.0.1
 
 pip install gitpython
 
