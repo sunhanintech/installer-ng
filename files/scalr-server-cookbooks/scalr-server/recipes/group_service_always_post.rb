c3e851eb9b9c43e271b419ce8aac7bc74b638ae3
Daniele Testa <daniele.testa@gmail.com>
2017-03-15 06:04:11 +0000

    Remove stdout logging for python services

--- files/scalr-server-cookbooks/scalr-server/recipes/group_service_always_post.rb
+++ files/scalr-server-cookbooks/scalr-server/recipes/group_service_always_post.rb
@@ -16,7 +16,7 @@ enabled_services(node, :python).each do |svc|
                     ' --verbosity=INFO' \
                     " #{svc[:service_extra_args]}" \
                     # Note: 'start' is added by the proxy.
-    stdout_logfile  "#{log_dir_for node, 'supervisor'}/#{name}.log"
+    stdout_logfile  "NONE"
     stderr_logfile  "#{log_dir_for node, 'supervisor'}/#{name}.err"
     autostart       true
     action          [:enable, :start]
