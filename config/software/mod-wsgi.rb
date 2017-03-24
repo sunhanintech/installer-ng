d16b8af61ce8235a95dbb6eae8afb3ad025d9e1f
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-03-24 14:03:12 +0100

    Fix env for mod_wsgi compilation

--- config/software/mod-wsgi.rb
+++ config/software/mod-wsgi.rb
@@ -15,7 +15,7 @@ license path: 'LICENSE'
 
 
 build do
-  env = with_standard_compiler_flags(with_embedded_path)
+  env = with_embedded_path
 
   command './configure' \
           " --prefix=#{install_dir}/embedded" \
