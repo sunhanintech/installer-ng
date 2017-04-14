8cf3e18a063f5b70b8299c19beefabd0812542a0
Daniele Testa <daniele.testa@gmail.com>
2017-04-14 00:47:27 +0000

    Fix parameters for Cloud Services Gateway

--- files/scalr-server-cookbooks/scalr-server/recipes/group_csg_enabled_post.rb
+++ files/scalr-server-cookbooks/scalr-server/recipes/group_csg_enabled_post.rb
@@ -1,11 +1,11 @@
 supervisor_service 'cloud-service-gateway' do
   description     "Cloud Service Gateway"
   command         "#{node[:scalr_server][:install_root]}/embedded/bin/mitmdump" \
-                  " -p #{node[:scalr_server][:csg][:cert]}" \
-                  " --cadir=#{node[:scalr_server][:install_root]}/embedded/scalr/app/etc/csg" \
-                  " -s #{node[:scalr_server][:install_root]}/embedded/scalr/app/python/fatmouse/csg/proxy.py" \
-                  ' -DFOREGROUND'
-  stdout_logfile  "#{node[:scalr_server][:install_root]}/var/log/service/csg.log"
+                  " -p #{node[:scalr_server][:csg][:bind_port]}" \
+                  " --cadir=#{node[:scalr_server][:install_root]}/etc/scalr/csg" \
+                  " -s #{node[:scalr_server][:install_root]}/embedded/scalr/app/python/fatmouse/csg/proxy.py"
+  stdout_logfile  "#{node[:scalr_server][:install_root]}/var/log/service/cloud-service-gateway.log"
+  environment     'PYTHONPATH' => "#{node[:scalr_server][:install_root]}/embedded/scalr/app/python/fatmouse"
   redirect_stderr true
   autostart       true
   startsecs       5
