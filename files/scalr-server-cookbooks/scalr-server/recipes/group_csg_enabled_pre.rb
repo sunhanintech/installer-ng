9bade7ae0e0b85dd9a1d821f51c0eeef7ae50a6d
Daniele Testa <daniele.testa@gmail.com>
2017-04-14 01:24:01 +0000

    Change config path for csg

--- files/scalr-server-cookbooks/scalr-server/recipes/group_csg_enabled_pre.rb
+++ files/scalr-server-cookbooks/scalr-server/recipes/group_csg_enabled_pre.rb
@@ -1,11 +1,11 @@
-directory "#{node[:scalr_server][:install_root]}/embedded/scalr/app/etc/csg" do
-  description "Create directory (#{node[:scalr_server][:install_root]}/embedded/scalr/app/etc/csg)"
+directory "#{node[:scalr_server][:install_root]}/etc/scalr/csg" do
+  description "Create directory (#{node[:scalr_server][:install_root]}/etc/scalr/csg)"
   owner     'root'
   group     'root'
   mode      0755
 end
 
-mitmproxy_cert = "#{node[:scalr_server][:install_root]}/embedded/scalr/app/etc/csg/mitmproxy-ca-cert.pem"
+mitmproxy_cert = "#{node[:scalr_server][:install_root]}/etc/scalr/csg/mitmproxy-ca-cert.pem"
 file mitmproxy_cert do 
   description "Generating mitmproxy certificate (#{mitmproxy_cert})"
   content IO.read(node[:scalr_server][:csg][:cert])
@@ -16,7 +16,7 @@ file mitmproxy_cert do
   notifies  :restart, 'supervisor_service[cloud-service-gateway]' if service_is_up?(node, 'csg')
 end
 
-mitmproxy_key = "#{node[:scalr_server][:install_root]}/embedded/scalr/app/etc/csg/mitmproxy-ca.pem"
+mitmproxy_key = "#{node[:scalr_server][:install_root]}/etc/scalr/csg/mitmproxy-ca.pem"
 file mitmproxy_key do
   description "Generating mitmproxy key (#{mitmproxy_key})"
   content IO.read(node[:scalr_server][:csg][:cert]) + IO.read(node[:scalr_server][:csg][:key])
