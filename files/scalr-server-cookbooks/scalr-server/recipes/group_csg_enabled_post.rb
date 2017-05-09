e2b7d1a68bf9fb4a752cc7db78744425ef454801
Daniele Testa <daniele.testa@gmail.com>
2017-05-09 15:34:35 +0000

    Bug fix

--- files/scalr-server-cookbooks/scalr-server/recipes/group_csg_enabled_post.rb
+++ files/scalr-server-cookbooks/scalr-server/recipes/group_csg_enabled_post.rb
@@ -2,7 +2,7 @@ supervisor_service 'cloud-service-gateway' do
   description     "Cloud Service Gateway"
   command         "#{node[:scalr_server][:install_root]}/embedded/bin/mitmdump" \
                   " -p #{node[:scalr_server][:csg][:bind_port]}" \
-                  " --app-host #{node[:scalr_server][:csg][:bind_host]}" \
+                  " --app-host csg" \
                   " --cadir=#{node[:scalr_server][:install_root]}/etc/scalr/csg" \
                   " -s #{node[:scalr_server][:install_root]}/embedded/scalr/app/python/fatmouse/csg/proxy.py"
   stdout_logfile  "#{node[:scalr_server][:install_root]}/var/log/service/cloud-service-gateway.log"
