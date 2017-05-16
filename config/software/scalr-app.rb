0801493203dd0dad63227525b76c343f072aded9
Daniele Testa <daniele.testa@gmail.com>
2017-05-16 16:13:53 +0000

    Update code version

--- config/software/scalr-app.rb
+++ config/software/scalr-app.rb
@@ -13,6 +13,9 @@ build do
   # Install dependencies using composer
   command "#{install_dir}/embedded/bin/php #{install_dir}/embedded/bin/composer.phar install --no-dev"
 
+  # Update code version
+  command "#{install_dir}/embedded/bin/php ./app/bin/update-code-version.php"
+
   # Copy the code to the ./scalr dir.
   command "mkdir -p #{install_dir}/embedded/scalr"
   command "rsync -a --delete --exclude=.git --exclude=.gitignore --exclude=.drone.yml --exclude=installer-ng --exclude=.releaseignore --exclude=pybuild ./ #{install_dir}/embedded/scalr"
