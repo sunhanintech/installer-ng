c0bf9d0a4c3ea3779e1e58900459d242f1dba421
Daniele Testa <daniele.testa@gmail.com>
2017-05-22 05:06:37 +0000

    Pin ohai to version 8.23.0

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -29,6 +29,11 @@ license url: "https://raw.githubusercontent.com/chef/chef/#{version}/LICENSE"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
+  gem 'install ohai' \
+      " --version '8.23.0'" \
+      " --bindir '#{install_dir}/embedded/bin'" \
+      " --no-ri --no-rdoc", env: env
+
   gem 'install net-ssh' \
       " --version '2.6.5'" \
       " --bindir '#{install_dir}/embedded/bin'" \
