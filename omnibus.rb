918872c9c0a6028f547b8222dfefbbc87c8edb55
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-08-02 20:18:32 +0200

    Remove -O2 from injected CFLAGS as it is now there by default

--- omnibus.rb
+++ omnibus.rb
@@ -9,7 +9,7 @@ append_timestamp false
 
 # Standard flags used by Debian for compilation (dpkg-buildflags)
 # Consider: https://wiki.debian.org/HardeningWalkthrough
-inject_cflags '-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2'
+inject_cflags '-g -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2'
 inject_ldflags '-Wl,-Bsymbolic-functions -Wl,-z,relro'
 
 # Note: access key, secret key, and region are needed when uploading.
